dadb7ed2a010b27a8621ceed734f779b2c68445a
Jigs2806 <98840394+Jigs2806@users.noreply.github.com>
2023-04-26 15:52:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
+API_ID       = 23647909
+API_HASH     = "9dfe9da0f1ad454cc7f4db231d015edb"
+BOT_TOKEN    = "6159980048:AAHX1IS6GTQAxjwG1r-32pgfW4YhwXeeMaE"
+SESSION      = "BQFo1qUApe9b2HC--3QSIpmV0GfRzVIMuoUTslwxhgdqe2ObBLrvZcE_VNxKPSnYX6lpXkWUq5Ar7ga9dqAlAHmhs-O60aFBYnEBpI0WJfYKSeyOor9YhLRGPXvfN9KTvTOVmsP4KqY6eK_juKNzmerBLBvZyJidKsGxb8gewH4ESV59Wvb3agfdMmLggeyDB79gmw8eD0Vqcxmsnvy3lBv2IT0q_NNt5Ss8h-xHbHC88qI17q7cTfzqhyXZUjXMjDRGA-K61czRht4D6V94D4g2mfXeWaKNL1M74cYdpIcRr1HE2eW-FDvsjUFAoFqQnq3Edc2744_71t_nbmBxvJz8BIv5vgAAAAFaOZmxAA"
+DATABASE_URI = "mongodb+srv://ItsJigs007:Jigs@123@cluster0.szmovs3.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001989030079
+ADMIN        = 5808691633
 CHANNEL      = "@GreyMatter_Bots"
