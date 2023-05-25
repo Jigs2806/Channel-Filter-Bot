ab3ed78775792b3ea59b297f56c39340288f3b65
Jigs2806 <98840394+Jigs2806@users.noreply.github.com>
2023-05-26 01:38:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 20315376
 API_HASH     = "b3bd79d39274918f3595ba62151ddd3e"
 BOT_TOKEN    = "5642044336:AAGf7SJoUf537-FiEcx-i27dVxHT3nUH5Eg"
 SESSION      = "BQE1_PAAgW2pJRz62RhflAML6qssGHM93HqvLQotQTlqUm0ikuNDHXoGHZkcaP45-X8WgmtSe1gIx57UWap4jcNOTRW_VZTwfZy0mkG0qhYlofp2QFAdGHt26nq0QDhZc7IpLedRCITVD8ZbYap0sLUC7YmOamsKYsVCGd0-YDu-poR5rdFhGGHgqwI1fgy3pvSWzp5siGferTAQNvmfRDOvN2bZVXs71zOM8omew7ud9nvJL8Ms_ffApOA8UC784D7ZNsLBZj3cz9AEz39vWve7lwhQmZIdYDvdTPMccOzDEN1OIEyom9gQJ7tvSTuDo1j1xX9pEZJfhKlbX5AjsJK_87NrTAAAAAFmX3UVAA"
-DATABASE_URI = "mongodb+srv://Jigs2806:Jigs2806@cluster0.fc4wy.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://Jigs007:9ffdace04@cluster0.ggd0bvy.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001723274804
 ADMIN        = 5180042169
 CHANNEL      = "@Cineheart_Movies"
