942170a3f1cfe17754d94119480891bb97e68e73
Jigs2806 <98840394+Jigs2806@users.noreply.github.com>
2023-10-27 23:21:12 +0530

    Update delete.py

--- utils/delete.py
+++ utils/delete.py
@@ -3,25 +3,27 @@ from .helpers import *
 from time import time
 from client import DlBot
 
-
-async def check_up(bot):   
-    _time = int(time()) 
+async def check_up(bot):
+    _time = int(time())
     all_data = await get_all_dlt_data(_time)
+    
     for data in all_data:
         try:
-           await bot.delete_messages(chat_id=data["chat_id"],
-                                     message_ids=data["message_id"])           
+            await bot.delete_messages(chat_id=data["chat_id"], message_ids=data["message_id"])
         except Exception as e:
-           err=data
-           err["❌ Error"]=str(e)
-           print(err)
+            error_info = {
+                "❌ Error": str(e),
+                "data": data
+            }
+            print(error_info)  # Log the error for debugging
+    
     await delete_all_dlt_data(_time)
 
 async def run_check_up():
-    async with DlBot as bot: 
-        while True:  
-           await check_up(bot)
-           await asyncio.sleep(1)
-    
-if __name__=="__main__":   
-   asyncio.run(run_check_up())
+    async with DlBot() as bot:
+        while True:
+            await check_up(bot)
+            await asyncio.sleep(1800)  # Sleep for 30 minutes (1800 seconds)
+
+if __name__ == "__main__":
+    asyncio.run(run_check_up())
